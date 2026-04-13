85a5d249610f8e3073a28994235446c4499d9b4b
salsa1012 <salsanurafipah693@gmail.com>
2026-04-13 13:36:42 +0700

    commit

---
+++ src/components/infosiswa.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  nama: String,
+  kelas: String,
+  jurusan: String,
+  pria: Boolean,
+})
+const isActive = true;
+const count = ref(0)
+</script>
+
+<template> 
+   <div class="infosiswa">
+  <p>Nama: {{ nama }}</p>
+  <p v-if="isActive">kelas : {{ kelas }}</p>
+  <p> gender : wanita</p>
+  </div>
+
+  <ul>
+    <li v-for="(user, index) in users" :key="index">{{ user }} </li>
+  </ul>
+  </template>
+<style src="./infosiswa.css"></style>
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style> 
