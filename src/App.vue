d5d74d5da96024f95fe0628c191050c3bd618cf6
fadhli133 <Muhammadfadhi579@gmail.com>
2026-04-13 13:35:58 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <navbar />
+  <main class="pt-5 mt-4">
+    <router-view />
+  </main>
+  <Footer />
+</template>
+
+<script setup>
+import navbar from './components/navbar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style scoped>
+.content {
+    padding: 80px 20px;
+}
+</style>
+
+<section id="contact">
+  <!-- isi contact -->
+</section>
---
+++ src/components/navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#"> B2B Champions🏆🏆</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navMenu"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navMenu">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/About">About</router-link>
+          </li>
+            <li class="nav-item">
+            <router-link class="nav-link" to="/Services">Services</router-link>
+            </li>
+            <li class="nav-item">
+            <router-link class="nav-link" to="/Contact">Contact</router-link>
+            </li>
+            <li class="nav-item">
+            <router-link class="nav-link" to="/Portfolio">Portfolio</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
