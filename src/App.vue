de95c510f3aba39bd62784e6f5ee77fdf98261f7
fadhli133 <Muhammadfadhi579@gmail.com>
2026-04-20 10:48:36 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
-  <navbar />
+  <Navbar />
+
   <main class="pt-5 mt-4">
     <router-view />
   </main>
+
   <Footer />
+
+  <section id="contact" class="content">
+    <h2>Contact</h2>
+    <p>Isi contact di sini</p>
+  </section>
 </template>
 
 <script setup>
-import navbar from './components/navbar.vue'
+import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 </script>
 
 <style scoped>
 .content {
-    padding: 80px 20px;
+  padding: 80px 20px;
 }
 </style>
-
-<section id="contact">
-  <!-- isi contact -->
-</section>
